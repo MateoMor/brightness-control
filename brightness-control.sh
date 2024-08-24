4756ea4cfd9c34d90c924cfaf2aeac0fe80128f5
MateoMor <mateomorales5558@gmail.com>
2024-08-24 13:46:25 -0500

    script comments and variables translated to english

--- brightness-control.sh
+++ brightness-control.sh
@@ -2,41 +2,41 @@
 
 # How to run: ~/scripts/brightness-control.sh value0-1
 
-# Obtener el identificador de la pantalla
+# Get the display identifier
 display=$(xrandr | grep " connected" | awk '{print $1}') 
 
-# Obtener el brillo actual
+# Get the current brightness
 current_brightness=$(xrandr --verbose | grep -i brightness | awk '{print $2}')
 
-# Verificar si se pudo obtener el brillo actual
+# Check if the current brightness was obtained
 if [ -z "$current_brightness" ]; then
-    echo "No se pudo obtener el brillo actual. Saliendo."
+    echo "Could not obtain current brightness. Exiting."
     exit 1
 fi
 
-# Verificar si se proporciona un valor de brillo como argumento
+# Check if a brightness value is provided as an argument
 if [ $# -eq 0 ]; then
-    echo "Uso: $0 [cantidad_de_brillo_a_sumar_o_restar]"
+    echo "Usage: $0 [brightness_amount_to_add_or_subtract]"
     exit 1
 fi
 
-# Obtener la cantidad de brillo a sumar o restar del primer argumento
+# Get the brightness amount to add or subtract from the first argument
 brightness_change=$1
 
-# Calcular el nuevo brillo sumando o restando la cantidad proporcionada
+# Calculate the new brightness by adding or subtracting the provided amount
 new_brightness=$(echo "$current_brightness + $brightness_change" | bc)
 
-# Verificar si el nuevo brillo está por encima de 1.0
+# Check if the new brightness is above 1.0
 if (( $(echo "$new_brightness > 1.0" | bc -l) )); then
     new_brightness=1.0
 fi
 
-# Verificar si el nuevo brillo está por debajo de 0.3
+# Check if the new brightness is below 0.3
 if (( $(echo "$new_brightness < 0.3" | bc -l) )); then
     new_brightness=0.3
 fi
 
-# Establecer el nuevo valor de brillo
+# Set the new brightness value
 xrandr --output $display --brightness $new_brightness
 
-echo "Brillo ajustado a $new_brightness"
+echo "Brightness adjusted to $new_brightness"
